b618c313f646b4624665aab3bd6bfe6664ea7298
Yulms <yminsk@gmail.com>
2020-04-21 09:59:34 +0300

    Добавляет переименование блоков c page-...

--- source/js/app.js
+++ source/js/app.js
@@ -1,13 +1,13 @@
 "use strict";
 
-const pageHeader = document.querySelector(".page-header");
+const pageHeader = document.querySelector(".header");
 // без включенного js меню будет показано по умолчанию
-pageHeader.classList.add("page-header--closed");
-const buttonToggle = document.querySelector(".page-header__toggle");
+pageHeader.classList.add("header--closed");
+const buttonToggle = document.querySelector(".header__toggle");
 buttonToggle.addEventListener("click", function(evt) {
   evt.preventDefault();
-  buttonToggle.classList.toggle("page-header__toggle--active");
+  buttonToggle.classList.toggle("header__toggle--active");
   console.log("pageHeader", pageHeader)
 
-  pageHeader.classList.toggle("page-header--closed");
+  pageHeader.classList.toggle("header--closed");
 })

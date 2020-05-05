49b92086b3e59384d80e686983b62b9439836248
Yulms <yminsk@gmail.com>
2020-05-05 17:34:25 +0300

    Добавляет svg4everybody

--- source/js/app.js
+++ source/js/app.js
@@ -5,23 +5,23 @@ const pageHeader = document.querySelector(".header");
 pageHeader.classList.add("header--closed");
 pageHeader.classList.remove("header--no-js");
 const buttonToggle = document.querySelector(".header__toggle");
-buttonToggle.addEventListener("click", function(evt) {
+buttonToggle.addEventListener("click", function (evt) {
   evt.preventDefault();
   buttonToggle.classList.toggle("header__toggle--active");
   pageHeader.classList.toggle("header--closed");
-})
+});
 
 
 let modalCart = document.querySelector(".modal--cart");
 const buttonsCartAdd = document.querySelectorAll(".product-card__button");
 for (let buttonCartAdd of buttonsCartAdd) {
-  buttonCartAdd.addEventListener("click", function(evt) {
+  buttonCartAdd.addEventListener("click", function (evt) {
     evt.preventDefault();
     modalCart.classList.remove("modal--closed");
-  })
+  });
 };
 
-modalCart.addEventListener("click", function(evt) {
+modalCart.addEventListener("click", function (evt) {
   // если клик по overlay
   if (evt.target.classList.contains("modal")) {
     evt.target.classList.add("modal--closed");
@@ -29,12 +29,12 @@ modalCart.addEventListener("click", function(evt) {
 });
 
 // закрытие окна по нажатию esc
-window.addEventListener("keydown", function(evt) {
+window.addEventListener("keydown", function (evt) {
   if (evt.keyCode === 27) {
     if (!modalCart) return;
     if (!modalCart.classList.contains("modal--closed")) {
       evt.preventDefault();
       modalCart.classList.add("modal--closed");
-    }
-  }
+    };
+  };
 });
